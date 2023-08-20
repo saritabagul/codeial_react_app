a58224f346da85a9ef27a7bbb472681bf689488f
sarita bagul <saritabagul06@gmail.com>
2023-08-21 00:02:34 +0530

    Adding or updating images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className={styles.postHeader}>
           <div className={styles.postAvatar}>
             <img
-              src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+              src="https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
               alt="user-pic"
             />
             <div>
@@ -20,7 +20,7 @@ const Home = () => {
           <div className={styles.postActions}>
             <div className={styles.postLike}>
               <img
-                src="https://image.flaticon.com/icons/svg/1077/1077035.svg"
+                src="https://cdn-icons-png.flaticon.com/128/2589/2589197.png"
                 alt="likes-icon"
               />
               <span>5</span>
@@ -28,7 +28,7 @@ const Home = () => {
 
             <div className={styles.postCommentsIcon}>
               <img
-                src="https://image.flaticon.com/icons/svg/1380/1380338.svg"
+                src="https://cdn-icons-png.flaticon.com/128/1380/1380338.png"
                 alt="comments-icon"
               />
               <span>2</span>
