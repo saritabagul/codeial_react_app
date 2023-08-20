b634b471c2436dfeb6e351cab18a93df52b092c3
sarita bagul <saritabagul06@gmail.com>
2023-08-21 00:43:13 +0530

    Props validations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import styles from '../styles/home.module.css';
+import propTypes from 'prop-types';
 
 const Home = ({posts}) => {
   return (
     <div className={styles.postsList}>
        {posts.map((post) => (
-      <div className={styles.postWrapper}>
+      <div className={styles.postWrapper} key={post._id}>
         <div className={styles.postHeader}>
           <div className={styles.postAvatar}>
             <img
@@ -57,5 +58,8 @@ const Home = ({posts}) => {
     </div>
   );
 };
+Home.propTypes = {
+  posts:propTypes.array.isRequired,
+}
 
 export default Home;
