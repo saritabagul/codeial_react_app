967d2c95845eb39114eac21062b83d3523122ad2
sarita bagul <saritabagul06@gmail.com>
2023-08-20 19:14:26 +0530

    setting up an api

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+import { getPosts } from '../api';
+
 function App() {
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      const response = await getPosts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+  },[]);
+
+
+
   return (
     <div className="App">
      <h1>Welcome to codeial</h1>
