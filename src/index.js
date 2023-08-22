d5918a151e3bfbe8293b8a5eee00260fb551bcc8
sarita bagul <saritabagul06@gmail.com>
2023-08-22 23:45:37 +0530

    Setting up api for login

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/index.css';
 import { App } from './components';
 import ReactDOM from 'react-dom/client';
-
+import {ToastProvider} from 'react-toast-notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // createRoot(container!) if you use TypeScript
 root.render( 
 <React.StrictMode>
-  <App />
+  <ToastProvider autoDismiss autoDismissTimeout={5000} placement='top-left'>
+    <App />
+  </ToastProvider>
 </React.StrictMode>
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,47 @@
 import styles from '../styles/login.module.css';
-
+import {useToasts} from 'react-toast-notifications';
+import { useState } from 'react';
+import { login } from '../api'; 
 const Login = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [loggingIn,setloggingIn] = useState(false);
+    const {addToast} = useToasts();
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        setloggingIn(true);
+        if(!email || !password){
+            return addToast('Please enter both email and password',
+            {appearance:'error'}); 
+        }
+
+        const response = await login(email,password);
+        if(response.success){
+          addToast('Successfully Logged In!',
+            {appearance:'success'});
+        }else{
+          addToast(response.message,
+          {appearance:'error'});
+        }
+        setloggingIn(false);
+    };
+
+
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>Log In</span>
 
       <div className={styles.field}>
-        <input type="email" placeholder="Email" required />
+        <input type="email" placeholder="Email" value={email} onChange={(e)=> setEmail(e.target.value)} />
       </div>
 
       <div className={styles.field}>
-        <input type="password" placeholder="Paasword" required />
+        <input type="password" placeholder="Paasword" value={password} onChange={(e)=> setPassword(e.target.value)} />
       </div>
 
-      <div className={styles.field}>
-        <button>Log In</button>
+      <div className={styles.field} >
+        <button disabled={loggingIn}>{loggingIn ? "Logging in..." :"Log In"}</button>
       </div>
     </form>
   );
