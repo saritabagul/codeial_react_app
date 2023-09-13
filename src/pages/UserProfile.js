36c26330813d48146259814505f1213d856144b1
sarita bagul <saritabagul06@gmail.com>
2023-09-14 00:09:54 +0530

    improving the logic of user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,10 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/settings.module.css";
 import { useAuth } from "../hooks";
 import { useToasts } from "react-toast-notifications";
+import { useParams,Navigate } from "react-router-dom";
+import { fetchUserProfile } from "../api";
+import { Loader } from "../components";
 
 const UserProfile = () => {
-  const user ={};
+  const [user,setUser] = useState({});
+  const [loading,setLoading] = useState(true);
+  const {userId} = useParams();
+
+  console.log('userId',userId);
+  const {addToast} = useToasts();
+
+  const  auth = useAuth();
+
+  // console.log("Auth", auth);
+
+  useEffect(()=>{
+    const getUser = async()=>{
+      const response = await fetchUserProfile(userId);
+      if(response.success){
+        setUser(response.data.user);
+      }else{
+        addToast(response.message,{
+          apperance:'error'
+        });
+        return <Navigate to="/" />;
+      }     
+      setLoading(false);
+    };
+
+    getUser();   
+  },[userId,addToast]);
+
+  if(loading){
+    return <Loader/>;
+  }
+
+  
+  const checkIfUserIsAFriend = () => {
+    const friends = auth.user.friendships;
+
+    // console.log(friends);
+    const friendIds = friends.map((friend) => friend.to_user._id);
+    const index = friendIds.indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+
+    return false;
+  }
+
+
   return (
     <div className={styles.settings}>
       <div className={styles.imgContainer}>
@@ -25,8 +75,11 @@ const UserProfile = () => {
       </div>
 
       <div className={styles.btnGrp}>
-        <button className={`button ${styles.saveBtn}`} > Add Friend </button>
-        <button className={`button ${styles.saveBtn}`} > Remove Friend </button>
+        {checkIfUserIsAFriend() ? (
+          <button className={`button ${styles.saveBtn}`}>Remove friend</button>
+        ) : (
+          <button className={`button ${styles.saveBtn}`}>Add friend</button>
+        )}
       </div>
     </div>
   );
