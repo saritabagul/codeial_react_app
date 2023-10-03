5f50c586d3759bc662f9dc218ee387660e59f6a7
sarita bagul <saritabagul06@gmail.com>
2023-10-04 00:07:14 +0530

    created friendlist

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,17 @@ import { useEffect, useState } from 'react';
 import { getPosts } from '../api';
 import styles from '../styles/home.module.css';
 import propTypes from 'prop-types';
-import { Comment } from '../components';
+import { Comment,FriendsList } from '../components';
 import { Link } from 'react-router-dom'
+import { useAuth } from '../hooks';
 
 
 
 const Home = () => {
-
+    
   const [posts,setPosts] = useState([]);
   const [Loading,setLoading] = useState(true);
+  const auth = useAuth();
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
@@ -27,52 +29,55 @@ const Home = () => {
   
 
   return (
-    <div className={styles.postsList}>
-       {posts.map((post) => (
-      <div className={styles.postWrapper} key={`post-${post._id}`}>
-        <div className={styles.postHeader}>
-          <div className={styles.postAvatar}>
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
-              alt="user-pic"
-            />
-            <div>
-              <Link to={`user/${post.user._id}`} className={styles.postAuthor}>{post.user.name}</Link>
-              <span className={styles.postTime}>a minute ago</span>
-            </div>
-          </div>
-          <div className={styles.postContent}>{post.content}</div>
-
-          <div className={styles.postActions}>
-            <div className={styles.postLike}>
+    <div className={styles.home}>
+      <div className={styles.postsList}>
+        {posts.map((post) => (
+        <div className={styles.postWrapper} key={`post-${post._id}`}>
+          <div className={styles.postHeader}>
+            <div className={styles.postAvatar}>
               <img
-                src="https://cdn-icons-png.flaticon.com/128/2589/2589197.png"
-                alt="likes-icon"
+                src="https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
+                alt="user-pic"
               />
-              <span>5</span>
+              <div>
+                <Link to={`user/${post.user._id}`} className={styles.postAuthor}>{post.user.name}</Link>
+                <span className={styles.postTime}>a minute ago</span>
+              </div>
             </div>
+            <div className={styles.postContent}>{post.content}</div>
 
-            <div className={styles.postCommentsIcon}>
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/1380/1380338.png"
-                alt="comments-icon"
-              />
-              <span>{post.comments.length}</span>
+            <div className={styles.postActions}>
+              <div className={styles.postLike}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/2589/2589197.png"
+                  alt="likes-icon"
+                />
+                <span>5</span>
+              </div>
+
+              <div className={styles.postCommentsIcon}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/1380/1380338.png"
+                  alt="comments-icon"
+                />
+                <span>{post.comments.length}</span>
+              </div>
             </div>
-          </div>
-          <div className={styles.postCommentBox}>
-            <input placeholder="Start typing a comment" />
-          </div>
-          <div className={styles.postCommentsList}>
-              {post.comments.map((comment) => (
-                <Comment comment={comment} />
-              ))}
+            <div className={styles.postCommentBox}>
+              <input placeholder="Start typing a comment" />
             </div>
-          
+            <div className={styles.postCommentsList}>
+                {post.comments.map((comment) => (
+                  <Comment comment={comment} />
+                ))}
+              </div>
+            
+          </div>
         </div>
+        )
+        )}
       </div>
-       )
-       )}
+      {auth.user && <FriendsList />}
     </div>
   );
 };
